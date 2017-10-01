18d3fa8832c75fdd7a18faf6800c5fe7a0844148
Jan Sorgalla <jsorgalla@gmail.com>
2017-10-01 16:38:25 +0200

    Reorder articles section

--- src/data.php
+++ src/data.php
@@ -86,9 +86,9 @@ return [
     ],
     'articles' => [
         [
-            'title' => 'Loïc Faugeron',
-            'description' => 'Super Speed Symfony - ReactPHP.',
-            'link' => 'https://gnugat.github.io/2016/04/13/super-speed-sf-react-php.html',
+            'title' => 'Sergey Zhuk',
+            'description' => 'A series of articles covering ReactPHP: from the basics to the real application examples.',
+            'link' => 'http://seregazhuk.github.io/reactphp-series',
         ],
         [
             'title' => 'Cees-Jan Kiewiet',
@@ -96,14 +96,14 @@ return [
             'link' => 'http://blog.wyrihaximus.net/categories/reactphp-series/',
         ],
         [
+            'title' => 'Loïc Faugeron',
+            'description' => 'Super Speed Symfony - ReactPHP.',
+            'link' => 'https://gnugat.github.io/2016/04/13/super-speed-sf-react-php.html',
+        ],
+        [
             'title' => 'Marc J. Schmidt',
             'description' => 'Bring High Performance Into Your PHP App (with ReactPHP).',
             'link' => 'http://marcjschmidt.de/blog/2014/02/08/php-high-performance.html',
-        ],
-        [
-            'title' => 'Sergey Zhuk',
-            'description' => 'A series of articles covering ReactPHP: from the basics to the real application examples.',
-            'link' => 'http://seregazhuk.github.io/reactphp-series',
         ],
     ],
     'talks' => [
