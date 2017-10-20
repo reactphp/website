0f44c07a125fdc5a6978622994735a4627a5987c
Jan Sorgalla <jsorgalla@gmail.com>
2017-10-20 15:05:57 +0200

    Use npm package for link preload polyfill

--- src/theme/assets/main-critical.js
+++ src/theme/assets/main-critical.js
@@ -1,6 +1,7 @@
 // Note, that critical javascript must not require any polyfills, eg. can't
 // use dynamic imports as webpack uses promises for that!
-require('./lib/rel-preload-as-style-polyfill');
+import polyfill from '@dotsunited/load-css-polyfill';
+polyfill();
 
 require('./fonts/fonts.css');
 require('./styles/styles-critical.css');
